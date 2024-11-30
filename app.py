be40dfec01f34368efbb9adf99d4dd27bb6d8858
riyazularfaat <98004322+riyazularfaat@users.noreply.github.com>
2024-11-30 17:48:07 +0000

    added flask file

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hellow_world():
+   return "Hello World!"
+
+if __name__ == '__main__':
+   app.run(host= '0.0.0.0', debug = True)
