d7bb5a7f7c2b8f1ed7e9074500375c62422a6fe8
riyazularfaat <98004322+riyazularfaat@users.noreply.github.com>
2024-12-01 16:49:57 +0000

    login page update

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hellow_world():
-   return "Hello World!"
+def login():
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(host= '0.0.0.0', debug = True)
